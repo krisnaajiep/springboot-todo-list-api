98e6c5635e993b6b899ba42e35c16c1006f348af
krisnaajiep <krisnaajiep@gmail.com>
2025-05-07 14:28:30 +0700

    feat(security): add JwtUtil class for JWT token generation and validation
    Introduce a utility class to handle JWT token operations, including generation, validation, subject extraction, and expiration checks.

---
+++ src/main/java/com/krisnaajiep/todolistapi/config/JwtConfig.java
@@ -0,0 +1,34 @@
+package com.krisnaajiep.todolistapi.config;
+
+/*
+IntelliJ IDEA 2025.1 (Ultimate Edition)
+Build #IU-251.23774.435, built on April 14, 2025
+@Author krisna a.k.a. Krisna Ajie
+Java Developer
+Created on 08/05/25 08.47
+@Last Modified 08/05/25 08.47
+Version 1.0
+*/
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+@Component
+@ConfigurationProperties(prefix = "jwt")
+@Setter
+@Getter
+@Validated
+public class JwtConfig {
+    @NotBlank
+    @Size(min = 32, max = 512)
+    private String secret;
+
+    @Min(1)
+    private long expiration;
+}
---
+++ src/main/java/com/krisnaajiep/todolistapi/security/JwtUtil.java
@@ -0,0 +1,65 @@
+package com.krisnaajiep.todolistapi.security;
+
+/*
+IntelliJ IDEA 2025.1 (Ultimate Edition)
+Build #IU-251.23774.435, built on April 14, 2025
+@Author krisna a.k.a. Krisna Ajie
+Java Developer
+Created on 07/05/25 09.32
+@Last Modified 07/05/25 09.32
+Version 1.0
+*/
+
+import com.krisnaajiep.todolistapi.config.JwtConfig;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    private final SecretKey key;
+    private final long expiration;
+
+    public JwtUtil(JwtConfig jwtConfig) {
+        this.key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtConfig.getSecret()));
+        this.expiration = jwtConfig.getExpiration();
+    }
+
+    public String generateToken(String subject) {
+        return Jwts.builder()
+                .subject(subject)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + expiration)) // 1 hour
+                .signWith(key)
+                .compact();
+    }
+
+    public boolean isTokenValid(String token, String identifier) {
+        String subject = extractSubject(token);
+        return subject.equals(identifier) && !isTokenExpired(token);
+    }
+
+    public String extractSubject(String token) {
+        return Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    private boolean isTokenExpired(String token) {
+        Date expiration = Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getExpiration();
+
+        return expiration.before(new Date());
+    }
+}
