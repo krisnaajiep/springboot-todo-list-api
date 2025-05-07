b16f1f09a2b5ae1160b6fb1bdd4f52514158dc84
krisnaajiep <krisnaajiep@gmail.com>
2025-05-07 14:26:22 +0700

    refactor(service): refactor AuthService to return String in register and login
    Change the return type of `register` and `login` methods in `AuthService` from `void` and `Optional<User>` to `String`.

--- src/main/java/com/krisnaajiep/todolistapi/service/AuthService.java
+++ src/main/java/com/krisnaajiep/todolistapi/service/AuthService.java
@@ -12,11 +12,8 @@ Version 1.0
 
 import com.krisnaajiep.todolistapi.dto.LoginRequestDto;
 import com.krisnaajiep.todolistapi.dto.RegisterRequestDto;
-import com.krisnaajiep.todolistapi.model.User;
-
-import java.util.Optional;
 
 public interface AuthService {
-    void register(RegisterRequestDto registerRequestDto);
-    Optional<User> login(LoginRequestDto loginRequestDto);
+    String register(RegisterRequestDto registerRequestDto);
+    String login(LoginRequestDto loginRequestDto);
 }
