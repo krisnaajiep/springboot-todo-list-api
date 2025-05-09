d618d16937a09dcaec1ef10f7c09224d63d8272c
krisnaajiep <krisnaajiep@gmail.com>
2025-05-09 08:55:01 +0700

    feat(exception): add LoginException and handler to manage unauthorized access
    - Introduced a custom LoginException class extending RuntimeException to handle login-related errors.
    - Updated GlobalExceptionHandler to include a dedicated handler for LoginException, returning an HTTP 401 Unauthorized response with a specific error message.
    - Cleaned up error handling logic by ensuring the `errors` map is cleared before processing in each handler.

---
+++ src/main/java/com/krisnaajiep/todolistapi/exception/LoginException.java
@@ -0,0 +1,17 @@
+package com.krisnaajiep.todolistapi.exception;
+
+/*
+IntelliJ IDEA 2025.1 (Ultimate Edition)
+Build #IU-251.23774.435, built on April 14, 2025
+@Author krisna a.k.a. Krisna Ajie
+Java Developer
+Created on 09/05/25 08.14
+@Last Modified 09/05/25 08.14
+Version 1.0
+*/
+
+public class LoginException extends RuntimeException {
+    public LoginException(String message) {
+        super(message);
+    }
+}
