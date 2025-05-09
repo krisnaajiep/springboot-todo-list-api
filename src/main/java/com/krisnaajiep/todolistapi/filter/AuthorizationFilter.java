86e7a171df668c16c8e578c271501e0a4a12ea9f
krisnaajiep <krisnaajiep@gmail.com>
2025-05-09 13:28:46 +0700

    feat(filter): add AuthorizationFilter to handle JWT-based request validation
    Implemented a filter to verify JWT tokens for incoming requests, excluding the /register and /login paths.

---
+++ src/main/java/com/krisnaajiep/todolistapi/filter/AuthorizationFilter.java
@@ -0,0 +1,75 @@
+package com.krisnaajiep.todolistapi.filter;
+
+/*
+IntelliJ IDEA 2025.1 (Ultimate Edition)
+Build #IU-251.23774.435, built on April 14, 2025
+@Author krisna a.k.a. Krisna Ajie
+Java Developer
+Created on 09/05/25 09.41
+@Last Modified 09/05/25 09.41
+Version 1.0
+*/
+
+import com.krisnaajiep.todolistapi.model.User;
+import com.krisnaajiep.todolistapi.repository.JdbcUserRepository;
+import com.krisnaajiep.todolistapi.security.JwtUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+@Component
+public class AuthorizationFilter extends OncePerRequestFilter {
+    private final JdbcUserRepository jdbcUserRepository;
+    private final JwtUtil jwtUtil;
+    private final static String[] EXCLUDED_PATHS = {"/register", "/login"};
+
+    public AuthorizationFilter(JdbcUserRepository jdbcUserRepository, JwtUtil jwtUtil) {
+        this.jdbcUserRepository = jdbcUserRepository;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        String path = request.getServletPath();
+
+        if (!Arrays.asList(EXCLUDED_PATHS).contains(path)) {
+            String authorizationHeader = request.getHeader("Authorization");
+
+            try {
+                if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+                    throw new Exception();
+                }
+
+                String token = authorizationHeader.substring(7);
+
+                if (jwtUtil.isTokenExpired(token)) {
+                    throw new Exception();
+                }
+
+                String subject = jwtUtil.extractSubject(token);
+                Integer userId = Integer.parseInt(subject);
+
+                User user = jdbcUserRepository.findById(userId).orElseThrow();
+
+                request.setAttribute("user", user);
+            } catch (Exception e) {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                response.setContentType("application/json");
+                response.getWriter().write("{\"message\": \"Unauthorized\"}");
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
