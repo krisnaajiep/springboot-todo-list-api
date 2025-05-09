a04822b5ddc075155a710bbdb55134f3d8cf254e
krisnaajiep <krisnaajiep@gmail.com>
2025-05-09 23:55:02 +0700

    feat(service): add pagination support to TaskService's findAll method
    - Updated the `findAll` method in `TaskService` to include pagination parameters (page and size) and return a `TasksResponseDto`.
    - Adjust the implementation in `TaskServiceImpl` to handle the updated method logic, including calculating the start index and mapping tasks to response DTOs.
    - Added a corresponding unit test to validate the new functionality.

--- src/main/java/com/krisnaajiep/todolistapi/service/TaskServiceImpl.java
+++ src/main/java/com/krisnaajiep/todolistapi/service/TaskServiceImpl.java
@@ -12,12 +12,15 @@ Version 1.0
 
 import com.krisnaajiep.todolistapi.dto.TaskRequestDto;
 import com.krisnaajiep.todolistapi.dto.TaskResponseDto;
+import com.krisnaajiep.todolistapi.dto.TasksResponseDto;
 import com.krisnaajiep.todolistapi.exception.ForbiddenException;
 import com.krisnaajiep.todolistapi.exception.TaskNotFoundException;
 import com.krisnaajiep.todolistapi.mapper.TaskMapper;
 import com.krisnaajiep.todolistapi.model.Task;
 import com.krisnaajiep.todolistapi.repository.JdbcTaskRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TaskServiceImpl implements TaskService {
@@ -64,7 +67,13 @@ public class TaskServiceImpl implements TaskService {
     }
 
     @Override
-    public Iterable<TaskResponseDto> findAll(Integer userId) {
-        return null;
+    public TasksResponseDto findAll(Integer userId, Integer page, Integer limit) {
+        Integer start = page > 1 ? (page - 1) * limit : 0;
+        List<Task> tasks = jdbcTaskRepository.findAll(userId, start, limit);
+
+        List<TaskResponseDto> taskResponseDtoList = tasks.stream().map(TaskMapper::toTaskResponseDto).toList();
+        int total = taskResponseDtoList.size();
+
+        return new TasksResponseDto(taskResponseDtoList, page, limit, total);
     }
 }
