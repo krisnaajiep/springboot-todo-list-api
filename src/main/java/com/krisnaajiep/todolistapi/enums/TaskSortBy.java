68c509888f0e4b964395f54b44111ed4c3099375
krisnaajiep <krisnaajiep@gmail.com>
2025-05-10 15:02:11 +0700

    feat(enums): add enums for task sorting: TaskSortBy and TaskSortDirection
    Introduced `TaskSortDirection` (ASC, DESC) and `TaskSortBy` (TITLE, CREATED_AT, UPDATED_AT) enums.

---
+++ src/main/java/com/krisnaajiep/todolistapi/enums/TaskSortBy.java
@@ -0,0 +1,17 @@
+package com.krisnaajiep.todolistapi.enums;
+
+/*
+IntelliJ IDEA 2025.1 (Ultimate Edition)
+Build #IU-251.23774.435, built on April 14, 2025
+@Author krisna a.k.a. Krisna Ajie
+Java Developer
+Created on 10/05/25 13.48
+@Last Modified 10/05/25 13.48
+Version 1.0
+*/
+
+public enum TaskSortBy {
+    TITLE,
+    CREATED_AT,
+    UPDATED_AT
+}
---
+++ src/main/java/com/krisnaajiep/todolistapi/enums/TaskSortDirection.java
@@ -0,0 +1,16 @@
+package com.krisnaajiep.todolistapi.enums;
+
+/*
+IntelliJ IDEA 2025.1 (Ultimate Edition)
+Build #IU-251.23774.435, built on April 14, 2025
+@Author krisna a.k.a. Krisna Ajie
+Java Developer
+Created on 10/05/25 13.51
+@Last Modified 10/05/25 13.51
+Version 1.0
+*/
+
+public enum TaskSortDirection {
+    ASC,
+    DESC
+}
