e78037dbe682c6be5b33059bf1dee88276c583c7
krisnaajiep <krisnaajiep@gmail.com>
2025-05-10 15:02:50 +0700

    feat(dto): add TasksRequestDto class for task query parameters
    This class introduces a DTO to handle task filtering and sorting. It includes fields for keyword search, sorting options, and pagination settings. Enums for the sort direction and criteria ensure consistent and type-safe input.

---
+++ src/main/java/com/krisnaajiep/todolistapi/dto/TasksRequestDto.java
@@ -0,0 +1,24 @@
+package com.krisnaajiep.todolistapi.dto;
+
+/*
+IntelliJ IDEA 2025.1 (Ultimate Edition)
+Build #IU-251.23774.435, built on April 14, 2025
+@Author krisna a.k.a. Krisna Ajie
+Java Developer
+Created on 10/05/25 00.03
+@Last Modified 10/05/25 00.03
+Version 1.0
+*/
+
+import com.krisnaajiep.todolistapi.enums.TaskSortBy;
+import com.krisnaajiep.todolistapi.enums.TaskSortDirection;
+import lombok.Data;
+
+@Data
+public class TasksRequestDto {
+    private String keyword = "";
+    private TaskSortBy sortBy = TaskSortBy.CREATED_AT;
+    private TaskSortDirection sortDir = TaskSortDirection.ASC;
+    private Integer page = 1;
+    private Integer limit = 10;
+}
