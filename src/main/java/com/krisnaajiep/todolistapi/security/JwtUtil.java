80aaf35d68b33d4e229aed490853747873e16cf5
krisnaajiep <krisnaajiep@gmail.com>
2025-05-13 11:56:06 +0700

    feat(util): update JwtUtil to support token purpose and validation
    - Refactored the JwtUtil class to handle access and refresh tokens separately, including distinct expiration times and purposes.
    - Added claims for purpose and familyId, as well as methods for extracting these values and validating token pairs.
    - Improved error handling for parsing and verifying tokens.

--- src/main/java/com/krisnaajiep/todolistapi/security/JwtUtil.java
+++ src/main/java/com/krisnaajiep/todolistapi/security/JwtUtil.java
@@ -11,55 +11,83 @@ Version 1.0
 */
 
 import com.krisnaajiep.todolistapi.config.JwtConfig;
-import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
+import java.util.UUID;
 
 @Component
 public class JwtUtil {
     private final SecretKey key;
-    private final long expiration;
+    private final long accessTokenExpiration;
+    private final long refreshTokenExpiration;
 
     public JwtUtil(JwtConfig jwtConfig) {
         this.key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtConfig.getSecret()));
-        this.expiration = jwtConfig.getExpiration();
+        this.accessTokenExpiration = jwtConfig.getAccessTokenExpiration();
+        this.refreshTokenExpiration = jwtConfig.getRefreshTokenExpiration();
     }
 
-    public String generateToken(String subject) {
-        return Jwts.builder()
+    public String generateToken(String subject, String purpose, String familyId) {
+        long expiration = purpose.equals("refresh")
+                ? refreshTokenExpiration
+                : accessTokenExpiration;
+
+        JwtBuilder jwtBuilder = Jwts.builder()
+                .id(UUID.randomUUID().toString())
                 .subject(subject)
                 .issuedAt(new Date())
-                .expiration(new Date(System.currentTimeMillis() + expiration)) // 1 hour
-                .signWith(key)
-                .compact();
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .claim("purpose", purpose)
+                .claim("familyId", familyId);
+
+        return jwtBuilder.signWith(key).compact();
     }
 
-    public boolean isTokenValid(String token, String identifier) {
-        String subject = extractSubject(token);
-        return subject.equals(identifier) && !isTokenExpired(token);
-    }
-
-    public String extractSubject(String token) {
-        return Jwts.parser()
-                .verifyWith(key)
-                .build()
-                .parseSignedClaims(token)
+    public String extractSubject(String token) throws Exception {
+        return getClaimsJws(token)
                 .getPayload()
                 .getSubject();
     }
 
-    public boolean isTokenExpired(String token) {
-        Date expiration = Jwts.parser()
-                .verifyWith(key)
-                .build()
-                .parseSignedClaims(token)
+    public String extractPurpose(String token) throws Exception {
+        return getClaimsJws(token)
+                .getPayload()
+                .get("purpose")
+                .toString();
+    }
+
+    public String extractFamilyId(String token) throws Exception {
+        return getClaimsJws(token)
+                .getPayload()
+                .get("familyId")
+                .toString();
+    }
+
+    public boolean isTokenExpired(String token) throws Exception {
+        Date expiration = getClaimsJws(token)
                 .getPayload()
                 .getExpiration();
 
         return expiration.before(new Date());
+    }
+
+    public boolean isTokenPairValid(String accessToken, String refreshToken) throws Exception {
+        return extractPurpose(accessToken).equals("access") &&
+                extractPurpose(refreshToken).equals("refresh") &&
+                extractFamilyId(accessToken).equals(extractFamilyId(refreshToken));
+    }
+
+    private Jws<Claims> getClaimsJws(String token) throws Exception {
+        try {
+
+            return Jwts.parser().verifyWith(key).build().parseSignedClaims(token);
+        } catch (JwtException | IllegalArgumentException e) {
+            throw new Exception("Unauthorized");
+        }
     }
 }
